63544ae07a43700457bbcdce285bdb88404f53d5
Pavlo Smahin <Pavlo_Smahin@epam.com>
2026-06-26 14:56:36 +0000

    feat: Add additional fields to the PackagePost model (#128)

--- src/main/java/org/folio/holdingsiq/model/PackagePost.java
+++ src/main/java/org/folio/holdingsiq/model/PackagePost.java
@@ -2,8 +2,11 @@ package org.folio.holdingsiq.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import java.util.List;
 import lombok.Builder;
 import lombok.Value;
+import org.folio.holdingsiq.deserializer.AlternateNameListDeserializer;
 
 @Value
 @Builder(toBuilder = true)
@@ -11,9 +14,33 @@ import lombok.Value;
 public class PackagePost {
 
   @JsonProperty("contentType")
-  private int contentType;
-  @JsonProperty("packageName")
-  private String packageName;
+  int contentType;
+
+  @JsonProperty("customAltNames")
+  @JsonDeserialize(using = AlternateNameListDeserializer.class)
+  List<AlternateName> customAltNames;
+
+  @JsonProperty("customDescription")
+  String customDescription;
+
+  @JsonProperty("customDisplayName")
+  String customDisplayName;
+
   @JsonProperty("customCoverage")
-  private CoverageDates coverage;
+  CoverageDates coverage;
+
+  @JsonProperty("packageFreeAccess")
+  Boolean packageFreeAccess;
+
+  @JsonProperty("packageName")
+  String packageName;
+
+  @JsonProperty("proxy")
+  Proxy proxy;
+
+  @JsonProperty("subjects")
+  List<Integer> subjectIds;
+
+  @JsonProperty("url")
+  String packageUrl;
 }
